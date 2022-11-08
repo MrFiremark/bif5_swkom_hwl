1c429889e31c77837c720c864430478657084a21
NOTEBOOK-WEI\Weijie <if21b077@technikum-wien.at>
2022-11-08 15:27:20 +0100

    Refactored Controller

--- src/main/java/at/fhtw/swen3/controller/rest/ParcelApiController.java
+++ src/main/java/at/fhtw/swen3/controller/rest/ParcelApiController.java
@@ -1,7 +1,6 @@
-package at.fhtw.swen3.services.impl;
+package at.fhtw.swen3.controller.rest;
 
 
-import at.fhtw.swen3.services.WarehouseApi;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.context.request.NativeWebRequest;
@@ -11,12 +10,12 @@ import javax.annotation.Generated;
 
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-09-14T11:21:04.473420Z[Etc/UTC]")
 @Controller
-public class WarehouseApiController implements WarehouseApi {
+public class ParcelApiController implements ParcelApi {
 
     private final NativeWebRequest request;
 
     @Autowired
-    public WarehouseApiController(NativeWebRequest request) {
+    public ParcelApiController(NativeWebRequest request) {
         this.request = request;
     }
 
--- src/test/java/at/fhtw/swen3/services/ParcelApiTest.java
+++ src/test/java/at/fhtw/swen3/services/ParcelApiTest.java
@@ -1,19 +1,18 @@
 package at.fhtw.swen3.services;
 
+import at.fhtw.swen3.controller.rest.ParcelApi;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import static org.mockito.Mockito.*;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class ParcelApiTest {
 
-    @Mock ParcelApi mocked;
+    @Mock
+    ParcelApi mocked;
 
     @Test
     void getRequest() {
